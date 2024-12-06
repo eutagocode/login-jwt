2cf45d1a490275006cd798d294ae14441214b291
eutagocode <thiagoluix21@gmail.com>
2024-12-06 12:19:35 -0300

    feat: :construction: Em construção a função de login e register onde registramos um usuário no banco de dados realizando a criptografação da senha e fazer login onde filtra o email para ver se a senha do banco de dados e do body são iguais para efetivar o login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,43 @@
-const register = (req, res) => {
-    res.send("register");
-    console.log("register");
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+
+const register = async (req, res) => {
+    const selectedUser = await User.findOne({ email: req.body.email });
+    if (selectedUser) return res.status(400).send("E-mail already exists.");
+
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: await bcrypt.hashSync(req.body.password),
+    });
+
+    try {
+        const userSaved = await user.save();
+        res.send(userSaved);
+    } catch (error) {
+        res.status(400).send(error);
+    }
 };
 
-const login = (req, res) => {
-    res.send("login");
-    console.log("login");
+const login = async (req, res) => {
+    const selectedUser = await User.findOne({ email: req.body.email });
+    if (!selectedUser)
+        return res.status(400).send("E-mail or Password incorrect.");
+
+    const passwordAndUserMatch = bcrypt.compareSync(
+        req.body.password,
+        selectedUser.password,
+    );
+
+    if (!passwordAndUserMatch)
+        return res.status(400).send("E-mail or Password incorrect.");
+
+    const token = jwt.sign({ _id: selectedUser._id }, process.env.TOKEN_SECRET);
+
+    res.header("authorization-token", token);
+
+    res.send("User logged");
 };
 
 export { register, login };
