19d14ab8e0f1b617dd322b8aa915232ab4f812ea
eutagocode <thiagoluix21@gmail.com>
2024-12-09 22:24:53 -0300

    feat: :sparkles: Foi importada a rota admin e utilizada como middleware nas rotas

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/userRouter.js";
+import adminRouter from "./routes/adminRouter.js";
 
 dotenv.config();
 const app = express();
 
 app.use("/user", express.json(), userRouter);
+app.use("/admin", express.json(), adminRouter);
 
 mongoose.connect(process.env.MONGO_CONNECTION_URL);
 
