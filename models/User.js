627f8ed773a6dc436dff23b0be4a4d59a99b75a1
eutagocode <thiagoluix21@gmail.com>
2024-12-09 22:28:09 -0300

    feat: :sparkles: Foi acrescentada a propriedade admin para identificar se é um usuário normal ou se é o admin

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = mongoose.Schema({
     name: { type: String, required: true, minlength: 3 },
     email: { type: String, required: true, minlength: 3 },
     password: { type: String, required: true, minlength: 8 },
+    admin: { type: Boolean, default: false },
     createdAt: { type: Date, default: Date.now },
 });
 
